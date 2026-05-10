e66aaf8645c80cdf627bd84dc17837117888a603
Daniel Soto <sotodaniel531@gmail.com>
2026-05-10 15:37:06 -0700

    display full chapter list when searching and selecting a manga of our choice

--- manga_scrape.py
+++ manga_scrape.py
@@ -58,9 +58,45 @@ def download_covers(results, folder="search_covers"):
         else:
             print(f"✗ Failed: {item['title']}")
 
+def get_manga_series(series_uuid):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
+    }
+
+    full_chapter_list_url = f"https://weebcentral.com/series/{series_uuid}/full-chapter-list"
+
+    response = requests.get(full_chapter_list_url, headers=headers)
+    soup = BeautifulSoup(response.text, "lxml")
+
+    for chapter in soup.find_all("div"):
+        parent_span = chapter.select_one("span.grow.flex")
+        if parent_span:
+            chapter_title = parent_span.find("span", class_="").get_text(strip=True)
+            print(chapter_title)
+    
+
 
 if __name__ == "__main__":
     search_query = input("Enter manga name to search: ")
     results = get_manga_list(search_query)
-    print(json.dumps(results, indent=4))
-    download_covers(results, folder="search_covers")
+    # print(json.dumps(results, indent=4))
+    # download_covers(results, folder="search_covers")
+    for i, result in enumerate(results):
+        print(f"{i+1}. {result['title']}")
+
+    if len(results) == 0:
+        print("No results found.")
+        exit()
+
+    while True:
+        choice = input(f"Enter the number of the manga to download ({1}-{len(results)}) or 'q' to quit: ")
+        if choice == "q":
+            break
+        if choice.isdigit() and 1 <= int(choice) <= len(results):
+            selected_result = results[int(choice) - 1]
+            print(f"Sending GET request to {selected_result['series_url']}...")
+            get_manga_series(selected_result['series_uuid'])
+            
+            # download_covers([selected_result], folder="search_covers")
+            break
+        print("Invalid input. Please enter a valid number or 'q' to quit.")
